2fa97f5a2c143a5bca41d230dd6ecd1e29d1f86c
123 321 <123@email.com>
2017-08-15 21:45:53 +0200

    added test-test

--- artifact1/src/test/java/project1/artifact1/AppTest.java
+++ artifact1/src/test/java/project1/artifact1/AppTest.java
@@ -1,38 +1,20 @@
 package project1.artifact1;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.testng.annotations.Test;
+import org.testng.Assert;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
+public class AppTest{
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
+	@Test
     public AppTest( String testName )
     {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+		Assert.assertTrue( true );
     }
 }
